d4d08a11cc2990e3a5393c6e7786bd4b91a0def4
Karel <paan.karel@gmail.com>
2018-10-20 22:40:10 +0300

    All minus API

---
+++ tour-of-heroes/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+
+// ng generate module app-routing --flat --module=app
+
+
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {path: 'detail/:id', component: HeroDetailComponent}
+];
+
+
+@NgModule({
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
+})
+
+
+export class AppRoutingModule {
+}
